b7f25c55c6004a3755dc5e26e8b2a7a42b80a22e
August Cornfeld <august.cornfeld@thelangschool.net>
2024-10-11 11:27:25 -0400

    no more debug, +mpp

--- MostPopularPost/src/Feed.java
+++ MostPopularPost/src/Feed.java
@@ -1,31 +1,34 @@
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Feed {
+
     private List<Post> posts;
-    public Feed(){
+
+    public Feed() {
         this.posts = new ArrayList<>();
     }
-    public void addAllPosts(List<Post> posts){
-        this.posts.addAll(posts);
-    }    
 
-    public void showFeed(){
-        for(Post post:posts){
+    public void addAllPosts(List<Post> posts) {
+        this.posts.addAll(posts);
+    }
+
+    public void showFeed() {
+        for (Post post : posts) {
             post.printPost();
         }
     }
 
-    public Post MostPopularPost(){
-        int mostLikes=0;
-        Post mostPopularSoFar=null;
-        for(Post post:posts){
-            if(post.getLikes()>mostLikes){
-                mostLikes=post.getLikes();
-                mostPopularSoFar=post;
+    public Post MostPopularPost() {
+        int mostLikes = 0;
+        Post mostPopularSoFar = null;
+        for (Post post : posts) {
+            if (post.getLikes() > mostLikes) {
+                mostLikes = post.getLikes();
+                mostPopularSoFar = post;
             }
         }
-        System.out.println(mostPopularSoFar);
         return mostPopularSoFar;
     }
     // TODO: create a mostPopularPost() method that returns the post with the most likes
