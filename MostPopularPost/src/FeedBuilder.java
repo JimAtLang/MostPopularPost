9a0b693e8b7ab9e53d9e79aba238aa617395a3a6
jimatlang <jim.stewart@thelangschool.org>
2024-10-30 13:06:42 -0400

    unimplemented chronological feed post

--- MostPopularPost/src/FeedBuilder.java
+++ MostPopularPost/src/FeedBuilder.java
@@ -1,3 +1,7 @@
 public class FeedBuilder {
-
+    public Feed chronologicalFeed(User user){
+        Feed f = new Feed();
+        // write code for chronological feed here
+        return f;
+    }
 }
