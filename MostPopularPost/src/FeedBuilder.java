4958357db1f9a65bbe3b0bd7b99f249229aaa759
August Cornfeld <august.cornfeld@thelangschool.net>
2024-10-30 14:13:32 -0400

    worked on chrono feed

--- MostPopularPost/src/FeedBuilder.java
+++ MostPopularPost/src/FeedBuilder.java
@@ -2,6 +2,10 @@ public class FeedBuilder {
     public Feed chronologicalFeed(User user){
         Feed f = new Feed();
         // write code for chronological feed here
+        ArrayList<Post> FollowedPosts posts;
+        for(User u:user.getFollows()){
+
+        }
         return f;
     }
 }
