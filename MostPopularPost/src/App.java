4bc99199edbcb0bc76e5e9839b6e3ac868434ed5
us riverside <us.riverside@thelangschool.net>
2024-11-12 14:12:53 -0500

    feedbuilder

--- MostPopularPost/src/App.java
+++ MostPopularPost/src/App.java
@@ -6,7 +6,11 @@ public class App {
         Feed f = new Feed();
         SocialMediaPostGenerator smpg = new SocialMediaPostGenerator();
         f.addAllPosts(smpg.generatePosts(5));
-        f.showFeed();
+        System.out.println("The most popular post for this feed is:");
+        Post mpp;
+        mpp = f.mostPopularPost();
+        mpp.printPost();
+
 
         //TODO: get the most popular post using the mostPopularPost() method of the Feed and then print it
     }
