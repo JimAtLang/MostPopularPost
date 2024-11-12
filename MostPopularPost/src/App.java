764c498c31403b251505b888774cc3e71743d1fd
Jim Stewart <jim.stewart@thelangschool.org>
2024-11-12 15:13:44 -0500

    chrono feed not alphabetical

--- MostPopularPost/src/App.java
+++ MostPopularPost/src/App.java
@@ -3,13 +3,9 @@ public class App {
         new App();
     }
     public App(){
-        Feed f = new Feed();
-        SocialMediaPostGenerator smpg = new SocialMediaPostGenerator();
-        f.addAllPosts(smpg.generatePosts(5));
-        //f.showFeed();
-        Post mp = f.mostPopularPost();
-        System.out.println("The most popular post is:");
-        mp.printPost();
+        Tester t = new Tester();
+        t.TestChronologicalFeed();
+        
         //TODO: get the most popular post using the mostPopularPost() method of the Feed and then print it
     }
 }
