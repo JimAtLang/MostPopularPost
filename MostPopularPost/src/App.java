b83e1e116d161992063fed2a1eb38b384f4e2345
August Cornfeld <august.cornfeld@thelangschool.net>
2024-11-13 13:26:25 -0500

    merged from main

--- MostPopularPost/src/App.java
+++ MostPopularPost/src/App.java
@@ -1,13 +1,16 @@
+
 public class App {
+
     public static void main(String[] args) throws Exception {
         new App();
     }
-    public App(){
+
+    public App() {
         Feed f = new Feed();
         SocialMediaPostGenerator smpg = new SocialMediaPostGenerator();
         f.addAllPosts(smpg.generatePosts(5));
         f.showFeed();
-        f.MostPopularPost();
+        f.mostPopularPost();
 
         //TODO: get the most popular post using the mostPopularPost() method of the Feed and then print it
     }
--- MostPopularPost/src/FeedBuilder.java
+++ MostPopularPost/src/FeedBuilder.java
@@ -1,11 +1,17 @@
+import java.util.ArrayList;
+
 public class FeedBuilder {
     public Feed chronologicalFeed(User user){
         Feed f = new Feed();
         // write code for chronological feed here
-        ArrayList<Post> FollowedPosts posts;
-        for(User u:user.getFollows()){
-
+        ArrayList<Post> followedPosts = new ArrayList<>();
+        for(User followedUser:user.getFollows()){
+            for(Post p:followedUser.getPostHistory()){
+                followedPosts.add(p);
+            }
         }
+        // TODO: sort list followedPosts chronologically
+        // TODO: add the list followedPosts to Feed
         return f;
     }
 }
